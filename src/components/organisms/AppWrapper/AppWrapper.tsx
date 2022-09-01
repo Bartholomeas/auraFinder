784ee43aea9797e8499eacb93da8cbe1c200fa8f
Bartholomeas <weivan321@gmail.com>
2022-09-01 20:26:52 +0200

    changed import route

--- src/components/organisms/AppWrapper/AppWrapper.tsx
+++ src/components/organisms/AppWrapper/AppWrapper.tsx
@@ -6,15 +6,15 @@ const AppWrapper = ({ children }: { children: JSX.Element | JSX.Element[] }) => 
 
 	let imageName = state.weather[0].icon.split('').splice(0, 2).join('') + 'i';
 	const images: any = {
-		'01i': "url('./public/img/01.jpg')",
-		'02i': "url('./public/img/02.jpg')",
-		'03i': "url('./public/img/03.jpg')",
-		'04i': "url('./public/img/04.jpg')",
-		'09i': "url('./public/img/09.jpg')",
-		'10i': "url('./public/img/10.jpg')",
-		'11i': "url('./public/img/11.jpg')",
-		'13i': "url('./public/img/13.jpg')",
-		'50i': "url('./public/img/50.jpg')",
+		'01i': "url('./../../../../public/img/01.jpg')",
+		'02i': "url('./../../../../public/img/02.jpg')",
+		'03i': "url('./../../../../public/img/03.jpg')",
+		'04i': "url('./../../../../public/img/04.jpg')",
+		'09i': "url('./../../../../public/img/09.jpg')",
+		'10i': "url('./../../../../public/img/10.jpg')",
+		'11i': "url('./../../../../public/img/11.jpg')",
+		'13i': "url('./../../../../public/img/13.jpg')",
+		'50i': "url('./../../../../public/img/50.jpg')",
 	};
 
 	return (
